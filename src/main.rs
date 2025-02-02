b40eaa00d03ebcd994ccceab0430d0c224551ed1
johan-stph <111685646+johan-stph@users.noreply.github.com>
2025-02-02 01:17:34 +0100

    fixed linting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
+use template::configuration::get_configuration;
+use template::run_server;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use template::configuration::get_configuration;
-use template::run_server;
 
 #[tokio::main]
 async fn main() {
     let configuration = get_configuration().expect("Failed to read configuration");
-    
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "template".into(),
-        std::io::stdout
-    );
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("template".into(), std::io::stdout);
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer);
     set_global_default(subscriber).expect("failed to set subscriber");
-    
-    
+
     let connection_pool: PgPool =
         PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
 
--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,15 +1,14 @@
-use axum::http::StatusCode;
 use axum::Json;
 use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Message {
-    message: String
+    message: String,
 }
 
 #[tracing::instrument]
 pub(crate) async fn health_check() -> Json<Message> {
     Json(Message {
-        message: "Hello from backend!".into()
+        message: "Hello from backend!".into(),
     })
 }
